c8e8d6dbdf9735038b8c052a9dd090077352f491
maybenicko <tronconiniccolo@gmail.com>
2025-03-11 09:21:53 +0100

    added main for exe creation (hidden in git)

--- DB_src/reddit_manager.py
+++ DB_src/reddit_manager.py
@@ -93,11 +93,3 @@ class RedditMonitor:
                             break
                     after = new_posts[-1].fullname
 
-
-if __name__ == "__main__":
-    reddit_monitor = RedditMonitor()
-    while True:
-        delay = 1800
-        reddit_monitor.fetch_posts()
-        print(f'[ SLEEPING FOR {delay}s... ]')
-        time.sleep(delay)
---
+++ main.py
@@ -0,0 +1,11 @@
+from DB_src.reddit_manager import RedditMonitor
+import time
+
+
+if __name__ == "__main__":
+    reddit_monitor = RedditMonitor()
+    while True:
+        delay = 1800
+        reddit_monitor.fetch_posts()
+        print(f'{reddit_monitor.get_time()} [ SLEEPING FOR {delay}s... ]')
+        time.sleep(delay)
